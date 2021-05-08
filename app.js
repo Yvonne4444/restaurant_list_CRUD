514f0a7019c94e9f08c3e3241566b40e96357848
Yvonne4444 <chester320@gmail.com>
2021-05-08 18:51:14 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+const db = mongoose.connection
+
+// mongoose
+mongoose.connect('mongodb://localhost/restaurantList', { useNewUrlParser: true, useUnifiedTopology: true })
+db.on('error', () => console.log('MongoDB error'))
+db.once('open', () => console.log('MongoDB connected'))
 
 app.get('/', (req, res) => {
   res.send('hi')
