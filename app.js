f8a973d794596b80ee5135391929432eb43ec5a1
Yvonne4444 <chester320@gmail.com>
2021-05-08 19:33:58 +0800

    feat: modify index.handlebars

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const Restaurant = require('./models/restaurant')
 const app = express()
 const port = 3000
 const db = mongoose.connection
 
+// handlebars
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
+// static files
 app.use(express.static('public'))
+// listen
+app.listen(port, () => console.log(`Express is listening on localhost:${port}`))
 
 // mongoose
 mongoose.connect('mongodb://localhost/restaurantList', { useNewUrlParser: true, useUnifiedTopology: true })
 db.on('error', () => console.log('MongoDB error'))
 db.once('open', () => console.log('MongoDB connected'))
 
+// index (瀏覽頁)
 app.get('/', (req, res) => {
-  res.render('index')
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log('error'))
 })
-
-app.listen(port, () => console.log(`Express is listening on localhost:${port}`))
