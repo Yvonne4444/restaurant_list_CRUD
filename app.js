2003a812491f08307b579b04f66d67cf76f8a762
Yvonne4444 <chester320@gmail.com>
2021-05-08 19:12:25 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 const db = mongoose.connection
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 // mongoose
 mongoose.connect('mongodb://localhost/restaurantList', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -10,7 +14,7 @@ db.on('error', () => console.log('MongoDB error'))
 db.once('open', () => console.log('MongoDB connected'))
 
 app.get('/', (req, res) => {
-  res.send('hi')
+  res.render('index')
 })
 
 app.listen(port, () => console.log(`Express is listening on localhost:${port}`))
