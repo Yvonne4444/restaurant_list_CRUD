9d6f5a423c9924f669c747ea3d47a18d7c1db65d
Yvonne4444 <chester320@gmail.com>
2021-05-08 19:52:48 +0800

    feat: add Create function

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 // static files
 app.use(express.static('public'))
+// body-parser
+app.use(express.urlencoded({ extended: true }))
 // listen
 app.listen(port, () => console.log(`Express is listening on localhost:${port}`))
 
@@ -24,5 +26,25 @@ app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log('error'))
+})
+
+// Create 頁面
+app.get('/create', (req, res) => {
+  return res.render('create')
+})
+// Create 資料處理
+app.post('/create/new', (req, res) => {
+  const restaurant = req.body
+  // 名稱為空格
+  if (restaurant.name.trim().length === 0) {
+    return res.redirect('/')
+  }
+  // 未提供圖片
+  if (restaurant.image.length === 0) {
+    restaurant.image = 'https://ubin.io/EtdNey'
+  }
+  return Restaurant.create(restaurant)
+    .then(() => res.redirect('/'))
     .catch(error => console.log('error'))
 })
