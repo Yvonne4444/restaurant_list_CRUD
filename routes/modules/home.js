7ef5c008133982cb19bffe2ed42717db60cc57d1
Yvonne4444 <chester320@gmail.com>
2021-05-15 10:51:32 +0800

    feat: add search function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,10 +2,28 @@ const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
+// home
 router.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
+})
+
+// search
+router.get('/search', (req, res) => {
+  const keyword = req.query.keyword // <input name="keyword">
+  const trimmedkeyword = keyword.trim()
+
+  return Restaurant.find({
+    $or: [
+      { name: { $regex: trimmedkeyword, $options: 'i' } },
+      { category: { $regex: trimmedkeyword, $options: 'i' } }
+      // i: case insensitive 無關大小寫
+    ]
+  })
+    .lean()
+    .then(restaurants => res.render('index', { restaurants, keyword }))
     .catch(error => console.log(error))
 })
 
